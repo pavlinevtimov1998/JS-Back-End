d2d6f523e6dbfb475c7ddfb44df0f8188dfdc7ed
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-24 22:15:37 +0300

    adding handlebars

--- Cubicle-Workshop/src/server.js
+++ Cubicle-Workshop/src/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const port = 3000;
 
 app.use("/static", express.static("public"));
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res.send("it works");
