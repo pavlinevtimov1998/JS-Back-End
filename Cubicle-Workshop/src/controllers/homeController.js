6ebe300de2779f6c145051e087b796a408b97111
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-25 14:13:33 +0300

    adding search functionality

--- Cubicle-Workshop/src/controllers/homeController.js
+++ Cubicle-Workshop/src/controllers/homeController.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const cubes = require("../db.json");
+const homeService = require("../services/homeService");
 
 router.get("/", (req, res) => {
+  const { search, from, to } = req.query;
+
+  const cubes = homeService.getCubes(search, from, to);
+
   res.render("index", { cubes });
 });
 
---
+++ Cubicle-Workshop/src/services/homeService.js
@@ -0,0 +1,29 @@
+const cubes = require("../db.json");
+
+exports.getCubes = (search = "", from, to) => {
+  const [fromLevel, toLevel] = searchHelper(from, to);
+
+  const result = cubes
+    .filter((c) =>
+      c.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    )
+    .filter(
+      (c) =>
+        Number(c.difficultyLevel) >= fromLevel &&
+        Number(c.difficultyLevel) <= toLevel
+    );
+
+  return result;
+};
+
+const searchHelper = (from, to) => {
+  if (from == undefined || from == "") {
+    from = 0;
+  }
+
+  if (to == undefined || to == "") {
+    to = 6;
+  }
+
+  return [Number(from), Number(to)];
+};
