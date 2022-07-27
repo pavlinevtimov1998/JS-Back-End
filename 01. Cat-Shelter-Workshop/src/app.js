75eab5ff8af6f6d661fe775b45f1b48c05ebae3f
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-27 15:03:48 +0300

    adding router and controllers

--- 01. Cat-Shelter-Workshop/src/app.js
+++ 01. Cat-Shelter-Workshop/src/app.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 
-const port = 3000;
+const router = require("./routes");
 
+const port = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("it works");
-});
+app.use("/public", express.static("public"));
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.use(router);
+
+// app.get("/cats/add-breed", (req, res) => {
+//   res.render("addBreed");
+// });
+
+// app.get("/cats/add-cat", (req, res) => {
+//   res.render("addCat");
+// });
 
 app.listen(port, () => console.log(`App listening on port ${port}...`));
