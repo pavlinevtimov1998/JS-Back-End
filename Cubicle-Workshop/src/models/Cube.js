c796459c6d61e50bcabb05ea5c6e59bff52c53dc
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-28 20:48:44 +0300

    create cube schema

---
+++ Cubicle-Workshop/src/models/Cube.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const cubeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 120,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function () {
+        return this.imageUrl.startsWith("http");
+      },
+      message: "Image should be a ling!",
+    },
+  },
+  difficultyLevel: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 6,
+  },
+  accessories: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Accessories",
+    },
+  ],
+});
+
+const Cube = mongoose.model("Cube", cubeSchema);
+
+exports.Cube = Cube;
