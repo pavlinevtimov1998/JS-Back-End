88c9230e6754383ac66ac5bca0a669a1d2e165ac
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-28 21:50:05 +0300

    adding create accessory functionality

---
+++ Cubicle-Workshop/src/controllers/createAccessoryController.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const accessoryService = require("../services/createAccessoryService");
+
+router.get("/create", (req, res) => {
+  res.render("accessory/create");
+});
+
+router.post("/create", async (req, res) => {
+  const accessoryData = req.body;
+
+  await accessoryService.create(accessoryData);
+
+  res.redirect("/");
+});
+
+module.exports = router;
--- Cubicle-Workshop/src/routes.js
+++ Cubicle-Workshop/src/routes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const homeController = require("./controllers/homeController");
-const createController = require("./controllers/createController");
+const createCubeController = require("./controllers/createCubeController");
 const detailsControler = require("./controllers/detailsController");
+const createAccessoryController = require("./controllers/createAccessoryController");
 
 router.use("/", homeController);
-router.use("/cube", createController);
+router.use("/cube", createCubeController);
 router.use("/details", detailsControler);
+router.use("/accessory", createAccessoryController);
 
 module.exports = router;
