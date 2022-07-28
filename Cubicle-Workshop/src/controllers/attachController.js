0566028e60a348f051a33779f648de2a2ee39d48
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-28 23:00:35 +0300

    adding back button functionality

--- Cubicle-Workshop/src/controllers/attachController.js
+++ Cubicle-Workshop/src/controllers/attachController.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const attachService = require("../services/attachService");
 
 router.get("/:cubeId/accessory", async (req, res) => {
+  const cubeId = req.params.cubeId;
+
   const accessories = await attachService.getAccessories().lean();
 
-  res.render("accessory/attach", { accessories });
+  res.render("accessory/attach", { accessories, cubeId });
 });
 
 router.post("/:cubeId/accessory", async (req, res) => {
