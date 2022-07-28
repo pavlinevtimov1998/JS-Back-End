d92010080250be16293e9b092a55b3eaffc59566
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-28 23:45:32 +0300

    adding about and 404 not found ciews

--- Cubicle-Workshop/src/routes.js
+++ Cubicle-Workshop/src/routes.js
@@ -3,12 +3,20 @@ const homeController = require("./controllers/homeController");
 const createCubeController = require("./controllers/createCubeController");
 const detailsControler = require("./controllers/detailsController");
 const createAccessoryController = require("./controllers/createAccessoryController");
-const attachController = require('./controllers/attachController');
+const attachController = require("./controllers/attachController");
 
 router.use("/", homeController);
 router.use("/cube", createCubeController);
 router.use("/details", detailsControler);
 router.use("/accessory", createAccessoryController);
-router.use('/attach', attachController);
+router.use("/attach", attachController);
+
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+router.all("*", (req, res) => {
+  res.render("404");
+});
 
 module.exports = router;
