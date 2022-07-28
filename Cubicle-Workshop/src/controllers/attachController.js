829f04437774fc987472d4bfab535151abe0d976
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-07-28 22:37:01 +0300

    adding attach functionality

--- Cubicle-Workshop/src/controllers/attachController.js
+++ Cubicle-Workshop/src/controllers/attachController.js
@@ -7,4 +7,13 @@ router.get("/:cubeId/accessory", async (req, res) => {
   res.render("accessory/attach", { accessories });
 });
 
+router.post("/:cubeId/accessory", async (req, res) => {
+  const cubeId = req.params.cubeId;
+  const accessoryId = req.body.accessory;
+
+  await attachService.attach(cubeId, accessoryId);
+
+  res.redirect(`/details/${cubeId}`);
+});
+
 module.exports = router;
--- Cubicle-Workshop/src/services/attachService.js
+++ Cubicle-Workshop/src/services/attachService.js
@@ -2,3 +2,14 @@ const Cube = require("../models/Cube");
 const Accessory = require("../models/Accessory");
 
 exports.getAccessories = () => Accessory.find();
+
+exports.attach = async (cubeId, accessoryId) => {
+  const cube = await Cube.findById(cubeId);
+  const accessory = await Accessory.findById(accessoryId);
+
+  cube.accessories.push(accessoryId);
+  accessory.cubes.push(cubeId);
+
+  await cube.save();
+  await accessory.save();
+};
