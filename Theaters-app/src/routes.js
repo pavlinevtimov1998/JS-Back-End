7d6cbd5306823d9a20e931cadf3292911a93286e
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-21 14:45:51 +0300

    Create routes.js

---
+++ Theaters-app/src/routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const homeController = require("./controllers/homeController");
+const authController = require("./controllers/authController");
+const theaterController = require("./controllers/theaterController");
+
+router.use("/", homeController);
+router.use("/auth", authController);
+router.use("/theater", theaterController);
+
+module.exports = router;
