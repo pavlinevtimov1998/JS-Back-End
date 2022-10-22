a416fb2f551ba4858cc688c007d26473bcb8ace9
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-22 16:12:51 +0300

    Update homeController.js

--- Theaters-app/src/controllers/homeController.js
+++ Theaters-app/src/controllers/homeController.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.render("home");
+const homeService = require("../services/homeService");
+
+router.get("/", async (req, res) => {
+  try {
+    if (!req.user) {
+      const plays = await homeService.getPlaysForGuest().lean();
+
+      return res.render("home", { plays });
+    }
+
+    const plays = await homeService.getAllPlays().lean();
+
+    res.render("home", { plays });
+  } catch (err) {
+    console.log(err);
+    res.locals.error = err.message;
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
