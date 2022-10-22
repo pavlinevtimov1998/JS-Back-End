274fe68895f15e74de8e06f022d06b7758dfc58e
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-22 16:13:11 +0300

    Create Play.js

---
+++ Theaters-app/src/models/Play.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const playSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: [50, "Description should be maximum 50 characters!"],
+    },
+    imageUrl: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    isPublic: {
+      type: Boolean,
+      default: false,
+    },
+    // ownerId: {
+    //   type: mongoose.Types.ObjectId,
+    //   required: true,
+    //   ref: "User",
+    // },
+    usersLikes: [
+      {
+        type: mongoose.Types.ObjectId,
+        default: [],
+        ref: "User",
+      },
+    ],
+    countLikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  { timestamps: { createdAt: "createdAt" } }
+);
+
+playSchema.pre("save", function (next) {
+  this.countLikes = this.usersLikes.length;
+
+  next();
+});
+
+const Play = mongoose.model("Play", playSchema);
+
+module.exports = Play;
