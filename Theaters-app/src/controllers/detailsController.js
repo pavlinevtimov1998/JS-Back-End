e175786c47b266703e88283d1a188615ceb40a52
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-23 21:37:19 +0300

    Create detailsController.js

---
+++ Theaters-app/src/controllers/detailsController.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const playService = require("../services/playService");
+// const { errorMessages } = require("../utils/validationMessages");
+
+router.get("/:playId", async (req, res) => {
+  const playId = req.params.playId;
+
+  try {
+    const play = await playService.getOne(playId);
+
+    res.render("theater/details", { play });
+  } catch (err) {
+    // const error = errorMessages(err);
+
+    // res.locals.error = error;
+
+    res.status(400).redirect("/");
+  }
+});
+
+module.exports = router;
