43e6e4dd92fa80bae30a6ee54ed3d0eaab31a5fe
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-23 21:46:56 +0300

    edit details

--- Theaters-app/src/controllers/detailsController.js
+++ Theaters-app/src/controllers/detailsController.js
@@ -7,9 +7,9 @@ router.get("/:playId", async (req, res) => {
   const playId = req.params.playId;
 
   try {
-    const play = await playService.getOne(playId);
+    const play = await playService.getOne(playId).lean();
 
-    res.render("theater/details", { play });
+    res.render("theater/details", play);
   } catch (err) {
     // const error = errorMessages(err);
 
