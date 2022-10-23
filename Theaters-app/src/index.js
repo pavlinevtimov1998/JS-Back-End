5a1f09f2b1222bfcb88fac940c881973a6d50f4b
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-23 11:04:57 +0300

    add isAuth middleware

--- Theaters-app/src/index.js
+++ Theaters-app/src/index.js
@@ -3,8 +3,9 @@ const cookieParser = require("cookie-parser");
 
 const { initDB } = require("./config/database");
 const { hbsConfig } = require("./config/hbs");
-const { PORT } = require("./constants");
+const { PORT, COOKIE_NAME } = require("./constants");
 const routes = require("./routes");
+const { isAuth } = require("./middlewares/isAuthMiddleware");
 
 async function startServer() {
   const app = express();
@@ -15,6 +16,8 @@ async function startServer() {
   app.use("/static", express.static("src/public"));
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
+
+  app.use(isAuth);
 
   app.use(routes);
 
