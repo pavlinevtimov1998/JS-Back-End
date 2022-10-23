6ac478f63f8ea2e271722cb3253dcc781d146fb8
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-23 11:05:31 +0300

    Update validationMessages.js

--- Theaters-app/src/utils/validationMessages.js
+++ Theaters-app/src/utils/validationMessages.js
@@ -1,4 +1,14 @@
 exports.errorMessages = (errors) =>
-  errors.map((e) => {
+  Object.values(errors).map((e) => {
     return { message: e.message };
   });
+
+exports.error = (message) => {
+  return {
+    errors: {
+      message,
+    },
+  };
+};
+
+exports.trimStr = (...arr) => arr.map((e) => e.trim());
