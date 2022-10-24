708c064baf8b00659ff9c99e7604d3cdc6519f2b
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-24 14:45:01 +0300

    Update createController.js

--- Theaters-app/src/controllers/createController.js
+++ Theaters-app/src/controllers/createController.js
@@ -1,19 +1,18 @@
 const router = require("express").Router();
 
-const editController = require("./editController");
-const detailsController = require("./detailsController");
-
 const playService = require("../services/playService");
 const { errorMessages } = require("../utils/validationMessages");
 
-router.get("/create", (req, res) => {
+router.get("/", (req, res) => {
   res.render("theater/create");
 });
 
-router.post("/create", async (req, res) => {
+router.post("/", async (req, res) => {
   const playData = req.body;
+  const userId = res.locals.user?._id;
+
   try {
-    await playService.createPlay(playData);
+    await playService.createPlay(playData, userId);
 
     res.redirect("/");
   } catch (err) {
@@ -25,8 +24,5 @@ router.post("/create", async (req, res) => {
     });
   }
 });
-
-router.use("/edit", editController);
-router.use("/details", detailsController);
 
 module.exports = router;
