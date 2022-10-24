58bd07f40fb5ce7c3c3cf53ffe26ecde176d3959
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-24 14:45:27 +0300

    Update routes.js

--- Theaters-app/src/routes.js
+++ Theaters-app/src/routes.js
@@ -3,9 +3,17 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController");
 const authController = require("./controllers/authController");
 const createController = require("./controllers/createController");
+const editController = require("./controllers/editController");
+const detailsController = require("./controllers/detailsController");
+const deleteController = require("./controllers/deleteController");
+const likeController = require("./controllers/likeController");
 
 router.use("/", homeController);
 router.use("/auth", authController);
-router.use("/theater", createController);
+router.use("/theater/create", createController);
+router.use("/theater/edit", editController);
+router.use("/theater/details", detailsController);
+router.use("/theater/like", likeController);
+router.use("/theater/delete", deleteController);
 
 module.exports = router;
