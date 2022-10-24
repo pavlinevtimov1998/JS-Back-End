560182a144f3de432db8425f1888d196f673cd2f
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-24 14:45:05 +0300

    Create deleteController.js

---
+++ Theaters-app/src/controllers/deleteController.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const playService = require("../services/playService");
+const { errorMessages } = require("../utils/validationMessages");
+
+router.get("/:playId", async (req, res) => {
+  const playId = req.params.playId;
+
+  try {
+    await playService.deletePlay(playId);
+
+    res.status(201).redirect("/");
+  } catch (err) {
+    const errors = errorMessages(err);
+
+    res.locals.errors = errors;
+
+    res.status(404).redirect("/");
+  }
+});
+
+module.exports = router;
