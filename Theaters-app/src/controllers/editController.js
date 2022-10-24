db377b7e042c81ccd9b2a3c85b081668ff3ec3c9
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-24 15:10:58 +0300

    Update editController.js

--- Theaters-app/src/controllers/editController.js
+++ Theaters-app/src/controllers/editController.js
@@ -8,23 +8,24 @@ router.get("/:playId", async (req, res) => {
   try {
     const play = await playService.getOne(playId);
 
-    
     res.render("theater/edit", play);
-  } catch (err) {}
-
+  } catch (err) {
+    res.redirect("/");
+  }
 });
 
 router.post("/:playId", async (req, res) => {
   const playData = req.body;
+  const playId = req.params.playId;
 
   try {
-    await playService.createPlay(playData);
+    await playService.editPlay(playData, playId);
 
     res.redirect("/");
   } catch (err) {
     const errors = errorMessages(err.errors);
 
-    res.status(404).render("theater/create", {
+    res.status(404).render("theater/edit", {
       playData,
       errors: errors,
     });
