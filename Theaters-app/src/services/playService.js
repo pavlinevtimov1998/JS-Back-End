b7eac7b2721455d27128e9616ee3de9e8620635c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-24 14:45:36 +0300

    Update playService.js

--- Theaters-app/src/services/playService.js
+++ Theaters-app/src/services/playService.js
@@ -2,13 +2,23 @@ const Play = require("../models/Play");
 
 exports.getOne = (playId) => Play.findById(playId);
 
-exports.createPlay = (body) => {
+exports.createPlay = (body, userId) => {
   const playData = {
     title: body.title,
     description: body.description,
     imageUrl: body.imageUrl,
     isPublic: body.isPublic == "on" ? true : false,
+    _ownerId: userId,
   };
 
   return Play.create(playData);
 };
+
+exports.likePlay = (playId, userId) =>
+  Play.findByIdAndUpdate(
+    playId,
+    { $push: { usersLikes: userId } },
+    { new: true }
+  );
+
+exports.deletePlay = (playId) => Play.findByIdAndDelete(playId);
