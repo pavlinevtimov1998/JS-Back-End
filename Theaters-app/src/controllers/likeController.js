2d66d2219003dfa60b75201d77d33ebb5313589d
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-24 14:45:19 +0300

    Create likeController.js

---
+++ Theaters-app/src/controllers/likeController.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+const playService = require("../services/playService");
+
+router.get("/:playId", async (req, res) => {
+  const playId = req.params.playId;
+  const userId = res.locals.user?._id;
+
+  try {
+    const play = await playService.likePlay(playId, userId).lean();
+
+    console.log(play.usersLikes);
+    console.log(userId);
+
+    res.redirect("/theater/details/" + playId);
+  } catch (err) {
+    res.redirect("/");
+  }
+});
+
+module.exports = router;
