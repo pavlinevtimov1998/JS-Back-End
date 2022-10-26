eede4e007bc0b73dc4bf175904d5b72f543df50d
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-26 15:22:07 +0300

    Update offerController.js

--- Real Estate Agency - App/src/controllers/offerController.js
+++ Real Estate Agency - App/src/controllers/offerController.js
@@ -27,16 +27,24 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.get("details/:offerId", isUser, async (req, res) => {
+router.get("/details/:offerId", isUser, async (req, res) => {
   const offerId = req.params.offerId;
   const userId = req.user._id;
 
   try {
-    const offer = await offerService.getOne(offerId).lean();
+    const offer = await offerService.getOne(offerId);
+    console.log(offer);
+    const isOwner = offer._ownerId == userId;
+    const isRenting = offer.usersRented.find((user) => user._id == userId);
+    const usersRenting = offer.usersRented.map((user) => user.name).join(" ,");
 
-    const isOwner = created._ownerId == userId;
-
-    res.render("details", { offer, isOwner });
+    res.render("details", {
+      offer: offer,
+      isOwner,
+      isRenting,
+      availableSpace: offer.availablePieces > 0,
+      usersRenting,
+    });
   } catch (err) {
     const error = errorMessages(err);
 
@@ -87,6 +95,23 @@ router.get("/delete/:offerId", async (req, res) => {
     await offerService.delItem(id);
 
     res.status(201).redirect("/");
+  } catch (err) {
+    const error = errorMessages(err);
+
+    res.locals.error = error;
+
+    res.status(404).redirect("/");
+  }
+});
+
+router.get("/rent/:offerId", isUser, async (req, res) => {
+  const offerId = req.params.offerId;
+  const userId = req.user._id;
+
+  try {
+    await offerService.rent(offerId, userId);
+
+    res.redirect("/offer/details/" + offerId);
   } catch (err) {
     const error = errorMessages(err);
 
