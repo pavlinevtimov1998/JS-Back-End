809104d0938bc828aefa5368a08aad4503f4f808
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-26 15:22:12 +0300

    Update offerService.js

--- Real Estate Agency - App/src/services/offerService.js
+++ Real Estate Agency - App/src/services/offerService.js
@@ -1,20 +1,49 @@
-// const Play = require("../models/Play");
+const Offer = require("../models/Offer");
 
-exports.getOne = (id) => Play.findOne({ _id: id });
+exports.getLastTreeOffers = () =>
+  Offer.find({})
+    .sort({ createdAt: -1 })
+    .select(
+      "-type -city -availablePieces -description -year -usersRented -updatedAt -createdAt -__v"
+    )
+    .limit(3)
+    .lean();
+
+exports.getAll = () => Offer.find().lean();
+
+exports.getOne = (offerId) =>
+  Offer.findById(offerId).populate("usersRented").lean();
 
 exports.create = (body, userId) => {
-  const body = {};
+  const offerData = {
+    name: body.name,
+    type: body.type,
+    city: body.city,
+    availablePieces: Number(body.availablePieces),
+    imageUrl: body.imageUrl,
+    description: body.description,
+    year: body.year,
+    _ownerId: userId,
+  };
 
-  // return Play.create(body);
+  return Offer.create(offerData);
 };
 
-exports.editPlay = (body, id) => {
-  const body = {};
+exports.edit = (body, offerId) => {
+  const offerData = {
+    name: body.name,
+    type: body.type,
+    city: body.city,
+    availablePieces: Number(body.availablePieces),
+    imageUrl: body.imageUrl,
+    description: body.description,
+    year: body.year,
+  };
 
-  return Play.findByIdAndUpdate(id, body, { runValidators: true });
+  return Offer.findByIdAndUpdate(offerId, offerData, { runValidators: true });
 };
 
-exports.deletePlay = (id) => Play.findByIdAndDelete(id);
+exports.delete = (offerId) => Offer.findByIdAndDelete(offerId);
 
 // exports.getSortedByLikes = () =>
 // Play.find({ isPublic: true }).sort({ countLikes: -1 });
@@ -22,9 +51,9 @@ exports.deletePlay = (id) => Play.findByIdAndDelete(id);
 // exports.getSortedByDate = () =>
 // Play.find({ isPublic: true }).sort({ createdAt: -1 });
 
-// exports.likePlay = (playId, userId) =>
-//   Play.findByIdAndUpdate(
-//     playId,
-//     { $push: { usersLikes: userId }, $inc: { countLikes: 1 } },
-//     { new: true }
-//   );
+exports.rent = (offerId, userId) =>
+  Offer.findByIdAndUpdate(
+    offerId,
+    { $push: { usersRented: userId }, $inc: { availablePieces: -1 } },
+    { new: true }
+  );
