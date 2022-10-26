e3c45986687a2f5c345860a6bdc45006d443c94f
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-27 00:15:04 +0300

    Update routes.js

--- Real Estate Agency - App/src/routes.js
+++ Real Estate Agency - App/src/routes.js
@@ -4,10 +4,14 @@ const authController = require("./controllers/authController");
 const homeController = require("./controllers/homeController");
 const offerController = require("./controllers/offerController");
 const catalogController = require("./controllers/catalogController");
+const searchController = require("./controllers/searchController");
+
+const { isUser } = require("./middlewares/guards");
 
 router.use("/", homeController);
 router.use("/catalog", catalogController);
 router.use("/auth", authController);
 router.use("/offer", offerController);
+router.use("/search", isUser, searchController);
 
 module.exports = router;
