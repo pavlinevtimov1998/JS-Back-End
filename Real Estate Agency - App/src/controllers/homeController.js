d9062faeff650f6a1cbcb1a9253962b5cc182837
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-26 15:22:06 +0300

    Update homeController.js

--- Real Estate Agency - App/src/controllers/homeController.js
+++ Real Estate Agency - App/src/controllers/homeController.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 
+const offerService = require("../services/offerService");
+
 router.get("/", async (req, res) => {
-  res.render("home");
+  try {
+    const offers = await offerService.getLastTreeOffers();
+
+    console.log(offers);
+    res.render("home", { offers, length: offers.length > 0 });
+  } catch (err) {
+    console.log(err);
+    res.render("home");
+  }
 });
+
 module.exports = router;
