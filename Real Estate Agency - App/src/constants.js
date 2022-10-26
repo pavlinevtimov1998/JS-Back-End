82b7ce62b1c6fcfb37dfa62a509bbeb06f55318c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-26 15:22:00 +0300

    Update constants.js

--- Real Estate Agency - App/src/constants.js
+++ Real Estate Agency - App/src/constants.js
@@ -1,2 +1,5 @@
 exports.PORT = 3000;
-exports.DATABASE_URL = "mongodb://localhost:27017/theater-app";
+exports.DATABASE_URL = "mongodb://localhost:27017/real-estate-app";
+exports.SALT = 10;
+exports.SECRET = "AJHSDJKHBASKDGHASD";
+exports.COOKIE_NAME = "token";
