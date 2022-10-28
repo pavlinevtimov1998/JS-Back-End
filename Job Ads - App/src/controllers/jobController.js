005c8b71fae8d427c2d31ff61545b9a412986cda
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-29 00:19:25 +0300

    add the rest of the primary exam

--- Job Ads - App/src/controllers/jobController.js
+++ Job Ads - App/src/controllers/jobController.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const jobService = require("../services/jobService");
+
 const { errorMessages } = require("../utils/errorMessages");
+const { isUser } = require("../middlewares/guards");
 
 router.get("/catalog", async (req, res) => {
   try {
@@ -16,13 +18,13 @@ router.get("/catalog", async (req, res) => {
   }
 });
 
-router.get("/create", (req, res) => {
+router.get("/create", isUser(), (req, res) => {
   res.render("create", {
     title: "Create Page",
   });
 });
 
-router.post("/create", async (req, res) => {
+router.post("/create", isUser(), async (req, res) => {
   const body = req.body;
   const userId = req.user._id;
 
@@ -40,17 +42,25 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.get("/details/:jobId", async (req, res) => {
+router.get("/details/:jobId", isUser(), async (req, res) => {
   const jobId = req.params.jobId;
-  const userId = req.user._id;
+  const userId = req.user?._id;
 
   try {
-    const job = await jobService.getOne(jobId);
+    const job = await jobService
+      .getOne(jobId)
+      .populate(["_ownerId", "usersApplied"])
+      .lean();
 
-    job.isOwner = job._ownerId._id.toString() == userId;
-    job.isApplied = job.usersApplied.find((id) => id.toString() == userId)
-      ? true
-      : false;
+    if (userId) {
+      job.isOwner = job._ownerId._id.toString() == userId;
+      job.isApplied = job.usersApplied.find(
+        (user) => user._id.toString() == userId
+      )
+        ? true
+        : false;
+      job.user = true;
+    }
 
     res.render("details", {
       title: "Details Page",
@@ -61,12 +71,68 @@ router.get("/details/:jobId", async (req, res) => {
   }
 });
 
-router.get("/edit/:jobId", (req, res) => {
-  res.render("catalog", {
-    title: "Catalog Page",
-  });
+router.get("/edit/:jobId", isUser(), async (req, res) => {
+  const jobId = req.params.jobId;
+  const userId = req.user._id;
+
+  try {
+    const job = await jobService.getOne(jobId).lean();
+
+    if (job._ownerId.toString() != userId) {
+      return res.redirect("/");
+    }
+
+    res.render("edit", {
+      title: "Edit Page",
+      job,
+    });
+  } catch (err) {
+    res.render("/");
+  }
 });
 
-router.post("/edit/:jobId", (req, res) => {});
+router.post("/edit/:jobId", isUser(), async (req, res) => {
+  const body = req.body;
+  const jobId = req.params.jobId;
+
+  try {
+    await jobService.edit(body, jobId);
+
+    res.redirect("/jobs/details/" + jobId);
+  } catch (err) {
+    const error = errorMessages(err);
+
+    res.render("create", {
+      body,
+      error,
+    });
+  }
+});
+
+router.get("/delete/:jobId", isUser(), async (req, res) => {
+  const jobId = req.params.jobId;
+  const userId = req.user._id;
+
+  try {
+    await jobService.deleteJob(jobId, userId);
+
+    res.redirect("/jobs/catalog");
+  } catch (err) {
+    res.redirect("/");
+  }
+});
+
+router.get("/apply/:jobId", isUser(), async (req, res) => {
+  const jobId = req.params.jobId;
+  const userId = req.user._id;
+
+  try {
+    await jobService.apply(jobId, userId);
+
+    res.redirect("/jobs/details/" + jobId);
+  } catch (err) {
+    res.redirect("/");
+  }
+});
 
 module.exports = router;
--- Job Ads - App/src/services/jobService.js
+++ Job Ads - App/src/services/jobService.js
@@ -1,10 +1,11 @@
 const Job = require("../models/Job");
+const { error } = require("../utils/errorMessages");
 
 exports.getFirstTree = () => Job.find().sort({ createdAt: 1 }).limit(3).lean();
 
 exports.getAll = () => Job.find().lean();
 
-exports.getOne = (_id) => Job.findOne({ _id }).populate("_ownerId").lean();
+exports.getOne = (_id) => Job.findOne({ _id });
 
 exports.create = (body, _ownerId) => {
   const job = {
@@ -18,13 +19,41 @@ exports.create = (body, _ownerId) => {
   return Job.create(job);
 };
 
-// exports.edit = (body, id) => {
-//   const body = {};
+exports.edit = (body, _id, _ownerId) => {
+  const job = {
+    headline: body.headline,
+    companyName: body.companyName,
+    description: body.description,
+    location: body.location,
+  };
 
-//   return Play.findByIdAndUpdate(id, body, { runValidators: true });
-// };
+  return Job.findOneAndUpdate({ _id, _ownerId }, job, { runValidators: true });
+};
 
-// exports.deleteItem = (id) => Play.findByIdAndDelete(id);
+exports.deleteJob = async (_id, _ownerId) => {
+  const result = await Job.findOneAndDelete({ _id, _ownerId });
+
+  if (!result) {
+    throw error("Unuthorized!");
+  }
+};
+
+exports.apply = async (_id, userId) => {
+  const job = await Job.findOne({ _id });
+
+  if (job.usersApplied.find((id) => id.toString() == userId)) {
+    throw error("You are already applied for this job!");
+  }
+
+  if (job._ownerId.toString() == userId) {
+    throw error("You can't apply for own post!");
+  }
+
+  return Job.findByIdAndUpdate(_id, {
+    $push: { usersApplied: userId },
+    $inc: { appliedCount: 1 },
+  });
+};
 
 // exports.getSortedByLikes = () =>
 // Play.find({ isPublic: true }).sort({ countLikes: -1 });
