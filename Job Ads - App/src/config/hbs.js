698b6f2e169fee3ec35b8ccb88ced541bf594b52
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-10-28 15:29:06 +0300

    Create hbs.js

---
+++ Job Ads - App/src/config/hbs.js
@@ -0,0 +1,12 @@
+const hbs = require("express-handlebars");
+
+exports.hbsConfig = (app) => {
+  app.engine(
+    "hbs",
+    hbs.engine({
+      extname: "hbs",
+    })
+  );
+  app.set("view engine", "hbs");
+  app.set("views", "src/views");
+};
